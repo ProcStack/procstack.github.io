6fb3c1523362d18079ee3cfb5d7a10bcf1f4dead
Kevin <trancor@metal-asylum.net>
2024-11-22 03:11:23 -0500

    Updates

--- js/ProckStackGitio.js
+++ js/ProckStackGitio.js
@@ -1,1 +1,51 @@
-import{pxlNav}from"./pxlNav.min.js";import{ProcstackPages}from"./ProcstackPages.js";const verbose=3,projectTitle="ProcStack.git.io",pxlRoomRootPath="./pxlRooms",startingRoom="CampfireEnvironment",bootRoomList=["CampfireEnvironment"],pxlNavr=new pxlNav(3,projectTitle,"./pxlRooms",startingRoom,bootRoomList),procPages=new ProcstackPages;function init(){procPages.init(),pxlNavr.bootTimer(),pxlNavr.init()}pxlNavr.subscribe("booted",procPages.postLoad.bind(procPages)),init();
+// ProcStack.Git.io Javascript
+//  Written by Kevin Edzenga; 2024
+//
+// -- -- -- -- -- -- -- -- -- -- -- -- -- --
+//
+//   This is an example implementation of `pxlNav` in a project;
+//     Tieing in `ProcstackPages` to manage the pages of the site,
+//       Listening to / triggering events on `pxlNav`
+//   For `pxlNav` scripting, the entry-point is `./Source/js/pxlNavCore.js`
+//
+
+import { pxlNav, pxlNavVersion } from './pxlNav.min.js';
+import { ProcstackPages } from './ProcstackPages.js';
+
+
+// Console logging level
+//   Options are - NONE, ERROR, WARN, INFO
+const verbose = 0
+
+// The Title of your Project
+//   This will be displayed on the 
+const projectTitle = "ProcStack.git.io";
+
+// pxlRoom folder path, available to change folder names or locations if desired
+const pxlRoomRootPath = "./pxlRooms";
+
+// Current possible rooms - "CampfireEnvironment"
+const startingRoom = "CampfireEnvironment"; 
+const bootRoomList = ["CampfireEnvironment"];//, "SaltFlatsEnvironment"];
+
+
+// -- -- -- -- --
+
+const pxlNavr = new pxlNav( verbose, projectTitle, pxlRoomRootPath, startingRoom, bootRoomList );
+
+// -- -- -- -- --
+
+const procPages = new ProcstackPages();
+pxlNavr.subscribe("booted", procPages.postLoad.bind(procPages));
+
+// -- -- --
+
+function init(){
+  procPages.init();
+  procPages.setPxlNavVersion(pxlNavVersion);
+
+  pxlNavr.bootTimer();
+  pxlNavr.init();
+  
+}
+(()=>{init()})();
