979b5b03150b7cdf1fcc264309d2fdb3b8da13cd
Kevin <trancor@metal-asylum.net>
2025-03-03 13:07:06 -0500

    Added `Documentation` section to `pxlNav` page

---
+++ docs/pages/pxlNav/documentation.js
@@ -0,0 +1,26 @@
+
+export const pageListingData = {
+  'name' : 'Documentation',
+  'media' : [
+    {
+      'type' : 'image',
+      'src' : '../pages/pxlNav/images/glslScriptEditor.webp',
+      'href' : './pxlNav-docs',
+      'alt' : 'pxlNav Shader Editor',
+      'style' : 'procPagesImageStyle',
+      'caption' : ["See pxlNav Documentation"]
+    
+    }
+  ],
+  'contentStyle' : [ 'procPagesCenterContent' ],
+  'content' : `
+    <div class="innerCenter">
+      <span class="textBump">View the <a href="./pxlNav-docs" target="_blank" alt="pxlNav Documentation" class="textBump">pxlNav Documentation</a></span>
+      <br>
+      <br>
+      Create your own interactive environments,
+      <br>Using <span class='textNudge'>pxlNav</span> for your Three.js projects.
+      <br><br>
+    </div>
+  `
+};
