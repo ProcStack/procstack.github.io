5289aba75519af9d329d37248a1c97bad2e22715
Kevin <trancor@metal-asylum.net>
2025-03-25 01:30:55 -0400

    Updates

--- pages/projects/pxlVisualizer.js
+++ pages/projects/pxlVisualizer.js
@@ -13,7 +13,7 @@ export const pageListingData = {
   'content' : `
     <a href='https://github.com/ProcStack/pxlVisualizer' class='procPagesRepoLinkStyle' target='_blank'>pxlVisualizer</a> <span class="textDrinkMeAlice textItalic">2019</span>
     <br><span class="textBump">Different switchable trippy visuals</span>
-    <br><span class="textShrink textItalic textName ">Language - <span class="textBold">C++ / OpenFrameWorks</span></span>
+    <br><span class="textShrink textItalic textName ">Language - <span class="textBold">C++ / OpenFrameWorks & GLSL</span></span>
     <div class="pppHBar"></div>
     
     Originally written in Python for PyGame, decided it would be a fun project to learn C++ on.
