7c3005a1e58d4989a56401052fde6b31f3224255
Kevin <trancor@metal-asylum.net>
2025-08-30 09:15:19 -0400

    Updates

--- pages/aiDev/aiNotes.js
+++ pages/aiDev/aiNotes.js
@@ -42,8 +42,7 @@ export const pageListingData = {
       These are my random thoughts or research on AI.
 
       <br>
-      <span class="showOnMobile"><br>Tap the Entry Title above to open the Blog Entry List.
-      <br>Or Tap the Left & Right arrow buttons to change Entry.</span>
+      <span class="showOnMobile"><br>Tap the Entry Title above to open the Blog Entry List.</span>
 
       <br>And no, I'm not using ai to speak for me here.
       <br>These are my thoughts, how ever scattered they may be.
