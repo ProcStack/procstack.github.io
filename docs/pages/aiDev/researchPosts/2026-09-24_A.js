143de2d758be82f3f6b7741475384e27b850b7ab
Kevin <trancor@metal-asylum.net>
2026-09-24 06:00:51 -0400

    'Language is the Tool' Blog Entry

---
+++ docs/pages/aiDev/researchPosts/2026-09-24_A.js
@@ -0,0 +1,162 @@
+import { baseEntryStruct, blogEntry } from '../../../js/blog/blogEntryBase.js';
+
+const entryData = baseEntryStruct();
+entryData.title = "Language is the Tool";
+entryData.date = "2026-09-24";
+entryData.eid = "A";
+entryData.tags = ["theory", "behavior", "language", "decision models"];
+entryData.body = `
+  Well, we are finally seeing what decision models can do,
+  <br>&nbsp;&nbsp; <span class="textName">JEV Neural Networks</span> are now a thing!
+
+  <br><br> And already the benefits of decision models are evident.
+
+  <br><br> Gotta love it,
+  <br>&nbsp;&nbsp; Like I said when BDH released last year,
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; Not enough research has been focused on situational awareness,
+  <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Environment Conceptualization & Intent
+
+  <br><br> These types of models give a path for conceptual model development.
+
+  <br><br> I'll take that undeserved boost to my ego for being on this line of reasoning for years now,
+  <br>&nbsp;&nbsp; This hasn't been the first time, but still feels good to know I'm not wasting my time...
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; Ego be damned, just lets reduce how much error I make.
+
+  <br><br> Gotta get the model trained up!
+  <br>&nbsp;&nbsp; And set up more of the environment...
+
+  <br><br><div class='procPagesAIDevBar'></div>
+
+  <br> I see this as evidence that language is just the tool, not the data the words are describing.
+  <br>&nbsp;&nbsp; I feel like that really didn't need to be stated,
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; But shows the serialized-data nature of language.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class="textItalic">I speak to this in my <span class="textName">A Fault</span> blog entry.</span>
+
+
+  <br><br><div class='procPagesAIDevBar'></div>
+
+
+  <br> I've started writing out the different dishes I've figured out for myself.
+  <br>&nbsp;&nbsp; Food recipes, if there is any confusion, haha
+  
+  <br><br> Before now, I didn't have the steps in a way I could just show someone
+  <br>&nbsp;&nbsp; More so, it's only the result of trial and error.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; Testing different amounts of things;
+  <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Figuring out what worked.
+
+  <br><br> I didn't have a way to describe making a dish I enjoy cooking for myself,
+  <br>&nbsp;&nbsp; But it's difficult to share that in an easy way.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; So we write recipes down.
+
+  <br><br> We'll describe the steps without measurements, when explained verbally,
+  <br>&nbsp;&nbsp; Leaving out the different ingredients we tried in different ways.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; The failed attempts and realizations along the way.
+
+  <br><br> You know what flavors you enjoy the most,
+  <br>&nbsp;&nbsp; But you don't convey that through text when you write recipes down.
+  <br>&nbsp;&nbsp; I mean... You get a massive story at the begining of recipes online,
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; Trying to convey some semblance of this...
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; It never really works and is kinda just noise, sadly.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; But is the purpose of it, in my mind.
+
+  <br><br> I don't ussually use recipes, except for baking,
+  <br>&nbsp;&nbsp; Baking can go bad real fast with experimentation.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; At my skill level...
+  <br>&nbsp;&nbsp; But at least I'm learning how to use baking soda/powder, flower, corn starch, such'n'such.
+
+  <br><br> Plus I enjoy the baking soda step in my salsas,
+  <br>&nbsp;&nbsp; It gets all fizzy from the vinegar & tomatoes in there.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; Things don't need to be too acidic.
+
+
+  <br><br><br> You can't fully convey that idea through text,
+  <br>&nbsp;&nbsp; Much like trying to describe a color.
+  <br>&nbsp;&nbsp; You can try, but it will never fully capture the experience.
+  
+  <br><br> Sunsets are warm and feel like a hug from a loved one...
+  <br>&nbsp;&nbsp; ...but for your eyes.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; It certainly leaves out a bit of the experience.
+
+  <br><br> The reasons we like the ingredients of the sunset can't be described with words,
+  <br>&nbsp;&nbsp; But easily understood through experience and concept.
+
+  <br><br> "It's like a sunrise, but in reverse... and less haze, with higher clouds."
+
+
+  <br><br><br><div class='procPagesAIDevBar'></div>
+
+
+  <br><br> This is the point of therapy, in my mind.
+  <br>&nbsp;&nbsp; To provide you with a lexicon to describe & understand your experiences.
+
+  <br><br> When someone doesn't have the words to describe their emotions & feelings,
+  <br>&nbsp;&nbsp; Things get frustrating.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; Frustration leads to annoyance
+  <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Eventually, anger.
+
+  <br><br> In the case of people suggesting someone to get therapy,
+  <br>&nbsp;&nbsp; Opposed to someone hearing that suggestion.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; There often seems to be a disconnect between perceived intent of the suggestion.
+  
+  <br><br> It seems those I've seen getting defensive to the suggestion of therapy seem mislead, course it's strangers online ussually,
+  <br>&nbsp;&nbsp; In person is ussually a "yeah yeah yeah" response.
+  
+  <br><br> Therapy allows you to describe yourself in a way that other's can understand,
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; And to see those descriptions in others around you.
+  <br>&nbsp;&nbsp; I think some people may not realize the strength of being able to speak of the self accurately.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; <span class='textDrinkMeAlice textItalic'>There's people who live by these labels in their lives...</span>
+  <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='textDrinkMeAlice textItalic'>And some weaponize the language...</span>
+  
+  <br><br> That idea of being emotionally intelligent, or what ever mixture of traits it aids.
+  <br>&nbsp;&nbsp; Conscientiousness, empathy, self-awareness, mindfulness, what have ya...
+
+  <br><br> I don't know if methods to address those emotions are necessarily a conclusion of therapy.
+  <br>&nbsp;&nbsp; Sometimes a label may be enough,
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; Others needing guidance though experiences.
+  <br>&nbsp;&nbsp; It's recieving the tools to navigate situations in the today & future
+  
+  <br><br> Again, psychology is just a hobby of mine, not a professional pursuit.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; I've read a couple books, like talking to people, and think about stuffs.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Much of which, reflected upon and written in sketch-books through-out the years.
+  
+
+  <br><br><br> While I haven't witnessed too many 'angry men' in my life,
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; I think I'm lucky in that, but people act differently in private...
+  <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Besides the situations I've needed to inject myself.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Been a couple times now.
+  <br>&nbsp;&nbsp; But I've heard enough people's first hand accounts, social media complaints, depictions in media,
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; and domestic violence & violent crime statistics,
+  <br>&nbsp;&nbsp; Even when considereing per capita.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; That, we men have a higher penchant to anger and violence.
+  <br>&nbsp;&nbsp; The few cause so many problems,
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; But it's enough of a problem that it's seen as an ever presence.
+
+  <br><br><br> The abilty to verbalize your state of mind and being,
+  <br>&nbsp;&nbsp; Is a result of going to therapy.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; It's not the purpose of therapy.
+
+  <br><br> I'm sure there are many interpretations of the purpose of therapy.
+  <br>&nbsp;&nbsp; As there are many needs for therapy in the first place.
+
+  <br><br> Ultimately, it's a system of description.
+
+
+  <br><br><br><div class='procPagesAIDevBar'></div>
+
+  <br> Transformers should be left to do what they do best,
+  <br>&nbsp;&nbsp; Orchestrate based on the data provided.
+  
+  <br><br> Interpreation of the environment/world, decisions, concepts,
+  <br>&nbsp;&nbsp; Can be handled outside of the transformer layer.
+  
+  <br><br> In many cases it seems outside senses are handled by the harness layer.
+  <br>&nbsp;&nbsp; But the transformer-based image & audio processing is further and further integrated into the core system these days.
+  <br>&nbsp;&nbsp;&nbsp;&nbsp; I need to investigate this current state of this tech a bit more.
+
+
+  <br><br><div class="textFullRight">- September 24th 2026</div>
+`;
+
+const blogEntryObj = new blogEntry(null, entryData);
+
+export { blogEntryObj };
